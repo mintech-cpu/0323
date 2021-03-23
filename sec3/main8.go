fee1f6fb623faf5165b4c2fde9d242f19d6c83db
mintech-cpu <78586667+mintech-cpu@users.noreply.github.com>
2021-03-23 23:01:21 +0900

    Add files via upload

---
+++ sec3/main8.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 明示的な定義
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "Hello"
+	fmt.Println(s)
+
+	var i3 int
+	var s3 string
+	fmt.Println(i3, s3)
+}
